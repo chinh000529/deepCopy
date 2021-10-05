cafa2d0bb34a0999bef998355d3d4f7337bff4c4
ddc.05 <chinh000529@gmail.com>
2021-10-05 18:50:17 +0700

    deep copy

---
+++ test.js
@@ -0,0 +1,55 @@
+function deepCopy(a){
+    if(a instanceof Object) {
+        let b = {};
+        const keys = Object.keys(a);
+        const values = Object.values(a);
+        for(let i = 0; i < values.length; i++) {
+            if(values[i] instanceof Array) {
+                let arr = [];
+                for(let j=0; j < values[i].length; j++){
+                    arr[j] = deepCopy(values[i][j])
+                }
+                b[keys[i]] = arr;
+            } else if(values[i] instanceof Object) {
+                b[keys[i]] = deepCopy(values[i]);
+            }
+            else {
+                b[keys[i]] = values[i];
+            }
+        }
+        return b;
+    }
+    else {
+        return a;
+    }
+}
+
+let obj1 = {
+    name: 'chinh',
+    age: 18,
+    // run: function(){
+    //     console.log('running...');
+    // },
+    pet: [
+        {
+            name: 'cun',
+            width: 5,
+        },
+        {
+            name: 'bin',
+            width: 3,
+        }
+    ],
+    phone: ['09854234', '03425634',],
+    mom: {
+        name: 'tam',
+        age: 40,
+        phone: ['0964736', '98257232'],
+    }
+}
+
+let obj2 =  deepCopy(obj1);
+
+console.log(obj1);
+console.log(obj2);
+console.log(obj1 === obj2);
